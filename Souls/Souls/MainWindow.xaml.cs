784ca7cae1962f88691493b2c6181b735c1109cd
toufic <tjhamadeh@gmail.com>
2017-02-16 13:55:11 -0500

    Some fields added to layout.

--- Souls/Souls/MainWindow.xaml.cs
+++ Souls/Souls/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace Souls
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int str;
+        private int str2Str
+        {
+            get { return str; }
+            set
+            {
+                str = value;
+                strBox.Text = value.ToString();
+            }
+        }
+        private int dex;
+        private int dex2Str
+        {
+            get { return dex; }
+            set
+            {
+                dex = value;
+                dexBox.Text = value.ToString();
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            comboBox.SelectedIndex = 0;
+            str2Str = 0;
+            dex2Str = 0;
         }
+
+        private void exitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void addStrButton_Click(object sender, RoutedEventArgs e)
+        {
+            str2Str++;
+        }
+
+        private void remStrButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (str2Str == 0)
+                return;
+            str2Str--;
+        }
+
+        private void addDexButton_Click(object sender, RoutedEventArgs e)
+        {
+            dex2Str++;
+        }
+
+        private void remDexButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dex2Str == 0)
+                return;
+            dex2Str--;
+        }
+
+    }
+
+    public enum SOULS : int
+    {
+        DARKSOULS  = 0,
+        DARKSOULS2 = 1,
+        DARKSOULS3 = 2,
+        BLOODBORNE = 3
     }
 }
