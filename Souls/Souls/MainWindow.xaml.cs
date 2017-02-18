fe9c18eb1dd7bb78c6727e322b9e6208b5ddfb7a
toufic <tjhamadeh@gmail.com>
2017-02-18 02:56:50 -0500

    Base stats added. Logic to handle stat changes added.

--- Souls/Souls/MainWindow.xaml.cs
+++ Souls/Souls/MainWindow.xaml.cs
@@ -20,6 +20,17 @@ namespace Souls
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BLOODBORNE bb;
+        private int lvl;
+        private int lvl2Str
+        {
+            get { return lvl; }
+            set
+            {
+                lvl = value;
+                lvlBox.Text = value.ToString();
+            }
+        }
         private int str;
         private int str2Str
         {
@@ -42,10 +53,9 @@ namespace Souls
         }
         public MainWindow()
         {
+
             InitializeComponent();
-            comboBox.SelectedIndex = 0;
-            str2Str = 0;
-            dex2Str = 0;
+            generateSoulList();
         }
 
         private void exitMenuItem_Click(object sender, RoutedEventArgs e)
@@ -53,37 +63,306 @@ namespace Souls
             Application.Current.Shutdown();
         }
 
+        private void strBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            int newStr;
+            if (e.Key == Key.Enter && int.TryParse(strBox.Text, out newStr))
+                checkStat(newStr, STATS_E.STRENGTH);
+        }
+
+        private void dexBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            int newDex;
+            if (e.Key == Key.Enter && int.TryParse(dexBox.Text, out newDex))
+                checkStat(newDex, STATS_E.DEXTERITY);
+        }
+
         private void addStrButton_Click(object sender, RoutedEventArgs e)
         {
+            if (str2Str >= 99)
+                return;
             str2Str++;
+            lvl2Str++;
         }
 
         private void remStrButton_Click(object sender, RoutedEventArgs e)
         {
-            if (str2Str == 0)
-                return;
+            switch ((SOULS_E)soulsList.SelectedIndex)
+            {
+                case SOULS_E.BLOODBORNE:
+                    if (str2Str <= bb.selClass.getStr())
+                    {
+                        str2Str = bb.selClass.getStr();
+                        // add level fixing logic ... ??? (actually maybe not)
+                        return;
+                    }
+                    else if (lvl2Str <= bb.selClass.getLvl())
+                    {
+                        lvl2Str = bb.selClass.getLvl();
+                        return;
+                    }
+                    break;
+                case SOULS_E.DARKSOULS:
+                    break;
+                case SOULS_E.DARKSOULS2:
+                    break;
+                case SOULS_E.DARKSOULS3:
+                    break;
+            }
+
             str2Str--;
+            lvl2Str--;
         }
 
         private void addDexButton_Click(object sender, RoutedEventArgs e)
         {
+            if (dex2Str >= 99)
+                return;
             dex2Str++;
+            lvl2Str++;
         }
 
         private void remDexButton_Click(object sender, RoutedEventArgs e)
         {
-            if (dex2Str == 0)
-                return;
+            switch ((SOULS_E)soulsList.SelectedIndex)
+            {
+                case SOULS_E.BLOODBORNE:
+                    if (dex2Str <= bb.selClass.getSkl())
+                    {
+                        dex2Str = bb.selClass.getSkl();
+                        return;
+                    }
+                    else if (lvl2Str <= bb.selClass.getLvl())
+                    {
+                        lvl2Str = bb.selClass.getLvl();
+                        return;
+                    }
+                    break;
+                case SOULS_E.DARKSOULS:
+                    break;
+                case SOULS_E.DARKSOULS2:
+                    break;
+                case SOULS_E.DARKSOULS3:
+                    break;
+            }
             dex2Str--;
+            lvl2Str--;
         }
 
+        private void soulsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            classList.Items.Clear();
+            classList.IsEnabled = true;
+            switch ((SOULS_E)soulsList.SelectedIndex)
+            {
+                case SOULS_E.BLOODBORNE:
+                    generateBloodborneList();
+                    break;
+                case SOULS_E.DARKSOULS:
+                    classList.IsEnabled = false;
+                    return;
+                case SOULS_E.DARKSOULS2:
+                    classList.IsEnabled = false;
+                    return;
+                case SOULS_E.DARKSOULS3:
+                    classList.IsEnabled = false;
+                    return;
+            }
+            classList.SelectedIndex = 0;
+        }
+
+        private void classList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            initializeClass(soulsList.SelectedIndex, classList.SelectedIndex);
+        }
+
+        private bool generateSoulList()
+        {
+            soulsList.SelectedIndex = 0;
+            soulsList.Items.Add("Bloodborne");
+            soulsList.Items.Add("Dark Souls");
+            soulsList.Items.Add("Dark Souls 2");
+            soulsList.Items.Add("Dark Souls 3");
+
+            return true;
+        }
+
+        private bool generateBloodborneList()
+        {
+            classList.Items.Add("Milquetoast");
+            classList.Items.Add("Lone Survivor");
+            classList.Items.Add("Troubled Childhood");
+            classList.Items.Add("Violent Past");
+            classList.Items.Add("Professional");
+            classList.Items.Add("Military Veteran");
+            classList.Items.Add("Noble Scion");
+            classList.Items.Add("Cruel Fate");
+            classList.Items.Add("Waste of Skin");
+
+            return true;
+        }
+
+        private bool initializeClass(int selectedSouls, int selectedClass)
+        {
+            switch ((SOULS_E)selectedSouls)
+            {
+                case SOULS_E.BLOODBORNE:
+                    bb = new BLOODBORNE(selectedClass);
+                    lvl2Str = bb.selClass.getLvl();
+                    str2Str = bb.selClass.getStr();
+                    dex2Str = bb.selClass.getSkl();
+                    // more to come ...
+                    break;
+                case SOULS_E.DARKSOULS:
+                    // not yet
+                    break;
+                case SOULS_E.DARKSOULS2:
+                    // not yet
+                    break;
+                case SOULS_E.DARKSOULS3:
+                    // not yet
+                    break;
+            }
+
+            return true;
+        }
+
+        private bool checkStat(int newStat, STATS_E stat)
+        {
+            int curStat;
+            int basStat;
+            switch (stat)
+            {
+                default:
+                case STATS_E.STRENGTH:
+                    if (newStat > 99 || newStat < bb.selClass.getStr())
+                    {
+                        str2Str = str;
+                        return false;
+                    }
+                    curStat = str2Str;
+                    basStat = bb.selClass.getStr();
+                    break;
+                case STATS_E.DEXTERITY:
+                case STATS_E.SKILL:
+                    if (newStat > 99 || newStat < bb.selClass.getSkl())
+                    {
+                        dex2Str = dex;
+                        return false;
+                    }
+                    curStat = dex2Str;
+                    basStat = bb.selClass.getSkl();
+                    break;
+            }
+
+            int diff = Math.Abs(curStat - newStat);
+            if (newStat > curStat)
+            {
+                lvl2Str += diff;
+            }
+            else if (newStat == curStat)
+            {
+                return false;
+            }
+            else
+            {
+                if ((lvl2Str - diff) < bb.selClass.getLvl())
+                {
+                    lvl2Str = lvl;
+                    newStat = curStat;
+                }
+                else if (newStat < basStat)
+                {
+                    lvl2Str = lvl;
+                    newStat = curStat;
+                }
+                else
+                {
+                    lvl2Str -= diff;
+                }
+            }
+
+            switch (stat)
+            {
+                default:
+                case STATS_E.STRENGTH:
+                    str2Str = newStat;
+                    break;
+                case STATS_E.DEXTERITY:
+                case STATS_E.SKILL:
+                    dex2Str = newStat;
+                    break;
+            }
+
+            return true;
+        }
     }
 
-    public enum SOULS : int
+    public enum STATS_E : int
     {
-        DARKSOULS  = 0,
-        DARKSOULS2 = 1,
-        DARKSOULS3 = 2,
-        BLOODBORNE = 3
+        STRENGTH = 0,
+        DEXTERITY,
+        SKILL
+    }
+
+    public enum SOULS_E : int
+    {
+        BLOODBORNE = 0,
+        DARKSOULS,
+        DARKSOULS2,
+        DARKSOULS3
+    }
+
+    public enum BLOODBORNE_E : int
+    {
+        MILQUETOAST = 0,
+        LONESURVIVOR,
+        TROUBLEDCHILDHOOD,
+        VIOLENTPAST,
+        PROFESSIONAL,
+        MILITARYVETERAN,
+        NOBLESCION,
+        CRUELFATE,
+        WASTEOFSKIN
+    }
+
+    public enum DARKSOULS1_E : int
+    {
+        WARRIOR = 0,
+        KNIGHT,
+        WANDERER,
+        THIEF,
+        BANDIT,
+        HUNTER,
+        SORCERER,
+        PYROMANCER,
+        CLERIC,
+        DEPRIVED
+    }
+
+    public enum DARKSOULS2_E : int
+    {
+        WARRIOR = 0,
+        KNIGHT,
+        SWORDSMAN,
+        BANDIT,
+        CLERIC,
+        SORCERER,
+        EXPLORER,
+        DEPRIVED
+    }
+
+    public enum DARKSOULS3_E : int
+    {
+        KNIGHT = 0,
+        MERCENARY,
+        WARRIOR,
+        HERALD,
+        THIEF,
+        ASSASSIN,
+        SORCERER,
+        PYROMANCER,
+        CLERIC,
+        DEPRIVED
     }
 }
---
+++ Souls/Souls/bloodborne.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souls
+{
+    class BLOODBORNE
+    {
+        public BBClass selClass;
+        public MILQUETOAST mtBase;
+        public LONESURVIVOR lsBase;
+        public TROUBLEDCHILDHOOD tcBase;
+        public VIOLENTPAST vpBase;
+        public PROFESSIONAL prBase;
+        public MILITARYVETERAN mvBase;
+        public NOBLESCION nsBase;
+        public CRUELFATE cfBase;
+        public WASTEOFSKIN wsBase;
+        public BLOODBORNE(int selectedClass)
+        {
+            switch ((BLOODBORNE_E)selectedClass)
+            {
+                case BLOODBORNE_E.MILQUETOAST:
+                    mtBase = new MILQUETOAST();
+                    selClass = mtBase;
+                    break;
+                case BLOODBORNE_E.LONESURVIVOR:
+                    lsBase = new LONESURVIVOR();
+                    selClass = lsBase;
+                    break;
+                case BLOODBORNE_E.TROUBLEDCHILDHOOD:
+                    tcBase = new TROUBLEDCHILDHOOD();
+                    selClass = tcBase;
+                    break;
+                case BLOODBORNE_E.VIOLENTPAST:
+                    vpBase = new VIOLENTPAST();
+                    selClass = vpBase;
+                    break;
+                case BLOODBORNE_E.PROFESSIONAL:
+                    prBase = new PROFESSIONAL();
+                    selClass = prBase;
+                    break;
+                case BLOODBORNE_E.MILITARYVETERAN:
+                    mvBase = new MILITARYVETERAN();
+                    selClass = mvBase;
+                    break;
+                case BLOODBORNE_E.NOBLESCION:
+                    nsBase = new NOBLESCION();
+                    selClass = nsBase;
+                    break;
+                case BLOODBORNE_E.CRUELFATE:
+                    cfBase = new CRUELFATE();
+                    selClass = cfBase;
+                    break;
+                case BLOODBORNE_E.WASTEOFSKIN:
+                    wsBase = new WASTEOFSKIN();
+                    selClass = wsBase;
+                    break;
+            }
+        }
+        public abstract class BBClass
+        {
+            public abstract int getLvl();
+            public abstract int getStr();
+            public abstract int getSkl();
+        }
+        public class MILQUETOAST : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public MILQUETOAST()
+            {
+                lvl = 10;
+                str = 12;
+                skl = 10;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class LONESURVIVOR : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public LONESURVIVOR()
+            {
+                lvl = 10;
+                str = 11;
+                skl = 10;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class TROUBLEDCHILDHOOD : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public TROUBLEDCHILDHOOD()
+            {
+                lvl = 10;
+                str = 9;
+                skl = 13;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class VIOLENTPAST : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public VIOLENTPAST()
+            {
+                lvl = 10;
+                str = 15;
+                skl = 9;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class PROFESSIONAL : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public PROFESSIONAL()
+            {
+                lvl = 10;
+                str = 9;
+                skl = 15;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class MILITARYVETERAN : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public MILITARYVETERAN()
+            {
+                lvl = 10;
+                str = 14;
+                skl = 13;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class NOBLESCION : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public NOBLESCION()
+            {
+                lvl = 10;
+                str = 9;
+                skl = 13;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class CRUELFATE : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public CRUELFATE()
+            {
+                lvl = 10;
+                str = 10;
+                skl = 9;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+        public class WASTEOFSKIN : BBClass
+        {
+            private int lvl;
+            private int str;
+            private int skl;
+            public WASTEOFSKIN()
+            {
+                lvl = 4;
+                str = 10;
+                skl = 9;
+            }
+            public override int getLvl()
+            {
+                return lvl;
+            }
+            public override int getStr()
+            {
+                return str;
+            }
+            public override int getSkl()
+            {
+                return skl;
+            }
+        }
+    }
+}
